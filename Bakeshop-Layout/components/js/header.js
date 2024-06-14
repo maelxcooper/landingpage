bf546487e625faac60efc9e5169892bac6c2cffc
Prince Caballes <prncaballes@proweaver.net>
2024-06-15 04:47:31 +0800

    Landing Page v1.2.9 06-14-24

--- Bakeshop-Layout/components/js/header.js
+++ Bakeshop-Layout/components/js/header.js
@@ -1,36 +1,28 @@
-// // Gsap Scrolltrigger
-// gsap.registerPlugin(ScrollTrigger);
-
-// function initializeAnimation() {
-//     gsap.from('header', {
-//         opacity: 0,
-//         delay: 1,
-//         y:-20
-//     })
-// }
-
 function initializeMenuToggle() {
     let menu = document.querySelector('#toggle_menu');
     let navbar = document.querySelector('.navbar');
-    let navBurger = document.querySelector('.hamburger-menu');
-    let navHold = document.querySelector('.toggle-holder');
+    let overLay = document.querySelector('.overlay');
+    let bodyCon = document.body;
+    let htmlCon = document.documentElement;
   
     menu.onclick = () => {
-        console.log('yawa')
-            menu.classList.toggle('bx-x');
-            navbar.classList.toggle('open');
+        console.log('yawa');
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+        overLay.classList.toggle('mob');
+        bodyCon.classList.toggle('blur');
+        htmlCon.classList.toggle('blur');
     };
     
     window.onresize = () => {
-        if (window.innerWidth < 1000) {
-            navHold.appendChild(navBurger);
-        } else {
-            navHold.insertAdjacentElement('afterend', navBurger);
-        }
-
-        if (window.innerWidth > 1000 && navbar.classList.contains('open')) {
-            menu.classList.toggle('bx-x');
+        if (window.innerWidth >= 1000 && navbar.classList.contains('open')) {
+            console.log('animal')
+            menu.classList.remove('bx-x');
            navbar.classList.remove('open');
+           overLay.classList.remove('mob');
+           bodyCon.classList.remove('blur');
+           htmlCon.classList.remove('blur');
+           
         }
     }
 
--- Bakeshop-Layout/components/js/headerAnim.js
+++ Bakeshop-Layout/components/js/headerAnim.js
@@ -1,7 +1,27 @@
-function initializeheaderAnimation() {
-    gsap.from('header', {
-        opacity: 0,
-        delay: 1,
-        y:-20
-    })
-}
+// function initializeheaderAnimation() {
+//     const startAnimation = () => {
+//         gsap.from('#header-container', {
+//             opacity: 0,
+//             delay: 1,
+//             y:-20
+//         })
+//     }
+    
+//     const stopAnimation = () => {
+//         gsap.killTweensOf('#header-container');
+//         gsap.set('#header-container', { clearProps: "all" }); 
+//     };
+
+//    if (window.innerWidth > 1000) {
+//     startAnimation();
+//    }
+
+//    window.onresize = () => {
+//         if (window.innerWidth < 1000) {
+//             stopAnimation();
+//         }
+//    }
+
+    
+// }
+
